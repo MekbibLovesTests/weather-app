070b1137052f068a5142390a3225385968a547fa
MekbibLovesTests <mekbibtare@gmail.com>
2023-11-10 14:36:39 +0300

    Display the weather data

--- src/createWeatherList.js
+++ src/createWeatherList.js
@@ -2,13 +2,11 @@ export default function createWeatherList(forecastDays) {
   const weatherList = [];
   forecastDays.forEach((forecast) => {
     weatherList.push({
-      maxtemp_c: forecast["day"]["maxtemp_c"],
-      maxtemp_f: forecast["day"]["maxtemp_f"],
-      mintemp_c: forecast["day"]["mintemp_c"],
-      mintemp_f: forecast["day"]["mintemp_f"],
-      avghumidity: forecast["day"]["avghumidity"],
+      "average temp in c": forecast["day"]["avgtemp_c"],
+      "average temp in f": forecast["day"]["avgtemp_f"],
+      "average Humidity": forecast["day"]["avghumidity"],
       condition: forecast["day"]["condition"]["text"],
-      date: forecast["day"]["date"],
+      date: forecast["date"],
     });
   });
   return weatherList;
--- src/index.js
+++ src/index.js
@@ -11,4 +11,43 @@ async function handleSubmit(e) {
   e.preventDefault();
   const weatherList = await getWeather(location);
   console.log(weatherList);
+  resetContainer();
+  weatherList.forEach((weather) => {
+    createWeatherCard(weather);
+  });
+}
+
+function createWeatherCard(weather) {
+  const container = document.querySelector(".weather");
+  const div = document.createElement("div");
+  const inputValue = document.querySelector(
+    "input[type = radio]:checked"
+  ).value;
+  let temperatureScale = "average temp in c";
+
+  if (inputValue === "fahrenheit") {
+    temperatureScale = "average temp in f";
+  }
+  div.append(
+    createSpanElement("date", weather),
+    createSpanElement(temperatureScale, weather),
+    createSpanElement("average Humidity", weather)
+  );
+  container.appendChild(div);
+}
+
+function createSpanElement(key, weather) {
+  const span = document.createElement("span");
+  const keyP = document.createElement("p");
+  keyP.textContent = key;
+  const valueP = document.createElement("p");
+  valueP.textContent = weather[key];
+  span.append(keyP, valueP);
+
+  return span;
+}
+
+function resetContainer() {
+  const container = document.querySelector(".weather");
+  container.textContent = "";
 }
